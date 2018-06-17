cf6cce4c0fd4b3a3898e8c658242a6ca99ae7b7d
alokmishraa <alokmishra.fms@gmail.com>
2018-06-17 15:44:46 +0530

    Added models

--- app/src/main/java/com/alokmishra/baadal/model/ForecastModel.java
+++ app/src/main/java/com/alokmishra/baadal/model/ForecastModel.java
@@ -1,4 +1,21 @@
+
 package com.alokmishra.baadal.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class ForecastModel {
+
+    @SerializedName("query")
+    @Expose
+    private Query query;
+
+    public Query getQuery() {
+        return query;
+    }
+
+    public void setQuery(Query query) {
+        this.query = query;
+    }
+
 }
